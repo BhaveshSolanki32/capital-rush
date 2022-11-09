f5a76cd73653057380995a6edf528d8599887853
Aman <solankibhavesh0149@gmail.com>
2022-11-09 06:11:38 +0530

    working on scroll

--- Assets/_scripts/sorting_stocks.cs
+++ Assets/_scripts/sorting_stocks.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sorting_stocks : MonoBehaviour
 {
     //handles sorting of element according to their holdings
     [SerializeField] GameObject _place_holder;
-
+    GameObject target;
+    int _current_index, _index_to_check;
 
     private void Start()
     {
@@ -30,33 +32,30 @@ public class sorting_stocks : MonoBehaviour
         transform.GetChild(0).GetComponent<stock_data>().on_stock_select(); // selects the first element
     }
 
-    public void onAddRemove_holdings(GameObject _target, int _AddOrRemove)
+    public void onAddRemove_holdings(GameObject _target, int _AddOrRemove)//_target holds the current selected stock,, _AddOrRemove can only be 1 or -1 tells if stock has been added or removed
     {
-        int _current_index, _index_to_check;
+        target = _target;
+        
 
         _current_index = _target.transform.GetSiblingIndex();
         _index_to_check = _current_index - _AddOrRemove;
-        print(transform.GetChild(_index_to_check).GetComponent<stock_data>()._holdings * _AddOrRemove < _target.GetComponent<stock_data>()._holdings * _AddOrRemove);
-        //
-        while (_index_to_check > 0 && _index_to_check < transform.childCount && transform.GetChild(_index_to_check).GetComponent<stock_data>()._holdings * _AddOrRemove < _target.GetComponent<stock_data>()._holdings * _AddOrRemove)
-        {
-            print("in");
-            _index_to_check -= _AddOrRemove;
-        }
 
-        
+
+        while (_index_to_check > 0 && _index_to_check < transform.childCount && transform.GetChild(_index_to_check).GetComponent<stock_data>()._holdings * _AddOrRemove < _target.GetComponent<stock_data>()._holdings * _AddOrRemove)
+            _index_to_check -= _AddOrRemove;
+
+
         if (_current_index - _AddOrRemove != _index_to_check)
         {
-            print("out");
             _place_holder.SetActive(true);
             _place_holder.transform.SetParent(transform);
-            _place_holder.transform.SetSiblingIndex((_AddOrRemove == 1) ? (_index_to_check+1) : (_index_to_check));
-            _target.transform.SetParent(transform.parent.parent);
-            StartCoroutine(moveTowards(_target));
+            _place_holder.transform.SetSiblingIndex((_AddOrRemove == 1) ? (_index_to_check + 1) : (_index_to_check));
+            _target.transform.SetParent(transform.parent);
+
+            StartCoroutine(moveTowards());
         }
 
     }
-
 
     //public void OnAdd_holdings(GameObject changed_target)
     //{
@@ -109,21 +108,41 @@ public class sorting_stocks : MonoBehaviour
 
     //}
 
-    IEnumerator moveTowards(GameObject target)//target holds the gameobject that needs to be moved,,target_post holds postion where it need to go
+    IEnumerator moveTowards()
     {
-        while ((int)target.transform.position.y/10 != (int)_place_holder.transform.position.y/10)
+        yield return new WaitForSeconds(0.1f);
+
+        Vector3 new_scroll_post, old_scroll_post = transform.position; //this helps compensate for if stock list is scrolled while lerping
+
+        while ((int)target.transform.position.y / 10 != (int)_place_holder.transform.position.y / 10)
         {
-            print(target.transform.position.y - _place_holder.transform.position.y);
-            target.transform.position = Vector3.Lerp(target.transform.position, _place_holder.transform.position, 0.1f);
+            new_scroll_post = transform.position;
+            target.transform.position = Vector3.Lerp(target.transform.position, _place_holder.transform.position, 0.1f) + new_scroll_post - old_scroll_post;
+            old_scroll_post = transform.position;
             yield return new WaitForSeconds(0.02f);
+
         }
 
-        print("doe");
+        while(!RectTransformUtility.RectangleContainsScreenPoint(transform.parent.GetComponent<RectTransform>(), target.transform.position))
+        {
+            if (transform.parent.position.y > target.transform.position.y)
+            {
+                transform.parent.GetComponent<ScrollRect>().verticalNormalizedPosition += 0.1f;
+            }
+            else
+            {
+                transform.parent.GetComponent<ScrollRect>().verticalNormalizedPosition -= 0.1f;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+      
         _place_holder.SetActive(false);
         target.transform.SetParent(transform);
         target.transform.SetSiblingIndex(_place_holder.transform.GetSiblingIndex());
         _place_holder.transform.SetParent(this.transform.parent.parent);
-        print("set");
+
+        
+
         StopCoroutine("moveTowards");
         yield return null;
     }
